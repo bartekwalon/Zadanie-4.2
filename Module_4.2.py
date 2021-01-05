0f6ec85c72bccebb2cae70f2eb4fd58e3417385c
bartekwalon <75641536+bartekwalon@users.noreply.github.com>
2021-01-05 09:05:01 +0100

    Updated after call

--- Module_4.2.py
+++ Module_4.2.py
@@ -1,23 +1,12 @@
-def sentencePalindrome(s): 
+import string
+def palindrome(str):
     """
         Checks if the string is a palindrome, based on entered data
     """
-    l, h = 0, len(s) - 1
-    s = s.lower()  
-    while (l <= h): 
-    
-        if (not(s[l] >= 'a' and s[l] <= 'z')): 
-            l += 1
-    
-        elif (not(s[h] >= 'a' and s[h] <= 'z')): 
-            h -= 1
-    
-        elif (s[l] == s[h]): 
-            l += 1
-            h -= 1
- 
-        else: 
-            return False
-
-    return True
-print(sentencePalindrome('Może jutro ta dama sama da tortu jeżom'))
+    alphabet=string.ascii_lowercase
+    temp=""
+    for c in str.lower():
+        if c in alphabet:
+            temp+=c
+    return temp==temp[::-1]
+print(palindrome('Może jutro ta dama sama da tortu jeżom.'))
